b39bd008c97b5785e1b3952c3c0cd7a36c6067ae
Aleksa Nikolic <aleksa.nikolic@intellya.ai>
2025-07-28 13:24:57 +0200

    add reusable button

--- angular-app/domaci_2/src/app/app.module.ts
+++ angular-app/domaci_2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar..component';
+import { ButtonComponent } from './components/button/button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBarComponent
+    NavBarComponent,
+    ButtonComponent
   ],
   imports: [
     BrowserModule,
